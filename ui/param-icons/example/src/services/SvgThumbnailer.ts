2d6b3f4e010c97dfbf3fed132aad5259ccbe9719
roberttodd <roberttodd@typedpath.com>
2024-07-17 15:35:49 +0100

    make thumbnailer call relative

--- ui/param-icons/example/src/services/SvgThumbnailer.ts
+++ ui/param-icons/example/src/services/SvgThumbnailer.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-//TODO fix this - should be relative
-const url = "https://devparamicons.testedsoftware.org/share/svgthumbnail"
+const urlPath = "/share/svgthumbnail"
 
 export default async function svgThumbnail(svg: String, id: string,  paramsIn: object) : Promise<String> {
-  //let urlWithParams = `url?id=${metaDataId}&params=selected=${strParams}`
+  let url = urlPath;
+  if (window.location.href.indexOf("localhost")>=0) {
+       url = 'https://devparamicons.testedsoftware.org' + urlPath
+  }
   let params = JSON.stringify(paramsIn)
   let res = await axios.post(url, svg, {headers : {
     'Content-Type': 'text/plain',
