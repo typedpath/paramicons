c438acadfd89d7f55378b7deb24bbc45c87470f3
roberttodd <roberttodd@typedpath.com>
2024-09-01 12:50:09 +0100

    tidy share and download button

--- ui/param-icons/example/src/Edit.tsx
+++ ui/param-icons/example/src/Edit.tsx
@@ -1,17 +1,13 @@
 import React, {Fragment, useEffect, useState} from 'react';
-import { Property,  MetaData, MultiShapeParams, transitionConMetaData, circlyIconMetaData, polarRadialCon2MetaData, blockconMetaData, polarRadialConMetaData, spiralConMetaData } from 'param-icons';
+import { Property,  MetaData, MultiShapeParams, circlyIconMetaData, polarRadialCon2MetaData, blockconMetaData, polarRadialConMetaData, spiralConMetaData } from 'param-icons';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
-import Typography from "@mui/material/Typography";
-import { ShareData, ShareView } from './Share';
 
 import ColourListSvg from './ColourListSvg';
-import ReactDOMServer from 'react-dom/server';
 
 import Stack  from '@mui/material/Stack';
-import svgThumbnail  from './functions/svgThumbnail';
+
 import NullableParamiconEdit from './NullableParamiconEdit';
 
 
@@ -31,7 +27,6 @@ const Edit = (editParams: EditParams) => {
   console.log(editParams);
   
   const [ params, setParams] = useState(paramsForEdit(editParams.params));
-  const [shareData, setShareData] = useState<ShareData | null>(null)
   const [frame, setFrame] = useState(0)
 
   function onChange() {
@@ -61,74 +56,13 @@ const Edit = (editParams: EditParams) => {
 });
 
 
-  const onClickShare = () => {
-      setShareData(shareData ? null : {link: null});
-      let newParams = {...params};
-      newParams.loading=!shareData;
-      setParams(newParams) ;
-  }
-
-  function renderAsSvgString(width: number, height: number) : string {
-    const renderPs = {...params} 
-    renderPs.pageHeight=height;
-    renderPs.pageWidth=width;
-console.log("renderAsSvgString ", width, height );
-    const el: JSX.Element = editParams.metaData.render(renderPs);
-    return  ReactDOMServer.renderToStaticMarkup(el);
-  }
-
-  const onClickDownload = () => {
-
-    let strEl = renderAsSvgString(600, 600)
-  
-    const blob = new Blob([strEl]);
-    const fileDownloadUrl = URL.createObjectURL(blob);
-  
-    const filename = 'download.svg';
-    const aTag = document.createElement('a');
-    aTag.href = fileDownloadUrl;
-    aTag.download = filename;
-    aTag.click();
-    URL.revokeObjectURL(fileDownloadUrl); 
-  
-  }  
-  
-
-
-
 function propertyUi(ps: Property) : JSX.Element{
    return  <Fragment>{edit(params, onChange, ps, ps.name)}</Fragment>
 }
  
-useEffect( () => {
-  if (shareData!=null && !shareData.link) {
-    // may need url encode !
-       let editParamsIn = `id=${editParams.metaData.id}&params=${encodeURI(JSON.stringify(params))}`          
-       svgThumbnail( editParamsIn, renderAsSvgString(600, 600))
-    
-    .then( (data) =>  
-    {
-    setShareData({link: data.toString()})
-    console.log('share.link:', data)
-    let newParams = {...params};
-    newParams.loading=false;
-    setParams(newParams) ;
-    }
-  );
-}
-}
-);
-
   const ui = <Stack>
-    <Button style={{textAlign: 'left'}} onClick={onClickDownload}>
-      <Typography variant="h4" >Download</Typography>
-        </Button>
-        <Button style={{textAlign: 'left'}} >
-      <Typography variant="h4"  onClick={onClickShare}>{shareData? "Edit" : "Share"}</Typography>
-        </Button>
-     {shareData && (shareData.link ? <ShareView link={shareData.link}></ShareView> : "building share link . . .")}   
-     {editParams.metaData.render(params)}
-     {!shareData && editParams.metaData.properties.map(ps => propertyUi(ps)) }
+      {editParams.metaData.render(params)}
+     { editParams.metaData.properties.map(ps => propertyUi(ps)) }
   </Stack>
   return ui;
   }
--- ui/param-icons/example/src/NullableParamiconEdit.tsx
+++ ui/param-icons/example/src/NullableParamiconEdit.tsx
@@ -1,12 +1,16 @@
 import { EditParams } from "./Edit";
 import { MetaData, MultiShapeParams } from 'param-icons';
-import React, {Fragment, useState}  from "react";
+import React, {Fragment, useState, useEffect}  from "react";
 import Edit from './Edit'
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 
 import Typography from "@mui/material/Typography";
 import { Button } from "@material-ui/core";
+
+import ReactDOMServer from 'react-dom/server';
+import { ShareData, ShareView } from "./Share";
+import svgThumbnail from "./functions/svgThumbnail";
 
 
 export interface NullableParamiconEditParams {
@@ -21,6 +25,8 @@ const NullableParamiconEdit = (nullableParams: NullableParamiconEditParams
   const [ inEdit, setInEdit] = useState( nullableParams.inEditDefault);
   const metaDatas = nullableParams.metaDatas;
   const editParams = nullableParams.editParams;
+  const [shareData, setShareData] = useState<ShareData | null>(null)
+
 
   const onClick = (metaData: MetaData, ps: MultiShapeParams) => {
     console.log("calling on change")
@@ -40,15 +46,72 @@ function toggleEdit() {
   setInEdit(!inEdit)
 }
 
+function renderAsSvgString(width: number, height: number, editParams: EditParams) : string {
+  const renderPs = {...editParams?.params} 
+  renderPs.pageHeight=height;
+  renderPs.pageWidth=width;
+console.log("renderAsSvgString ", width, height );
+  const el: JSX.Element = editParams.metaData.render(renderPs);
+  return  ReactDOMServer.renderToStaticMarkup(el);
+}
+
+const onClickDownload = () => {
+
+  let strEl = renderAsSvgString(600, 600, editParams!!)
+
+  const blob = new Blob([strEl]);
+  const fileDownloadUrl = URL.createObjectURL(blob);
+
+  const filename = 'download.svg';
+  const aTag = document.createElement('a');
+  aTag.href = fileDownloadUrl;
+  aTag.download = filename;
+  aTag.click();
+  URL.revokeObjectURL(fileDownloadUrl); 
+
+}  
+
+const onClickShare = () => {
+  setShareData(shareData ? null : {link: null});
+  //console.log("onClickShare: shareData:", shareData)
+  //let newParams = {...params};
+  //newParams.loading=!shareData;
+  //setParams(newParams) ;
+}
+
+
+useEffect( () => {
+
+  if (shareData!=null && !shareData.link && editParams) {
+     
+    // may need url encode !
+       let editParamsIn = `id=${editParams.metaData.id}&params=${encodeURI(JSON.stringify(editParams.params))}`          
+       svgThumbnail( editParamsIn, renderAsSvgString(600, 600, editParams))
+    
+    .then( (data) =>  
+    {
+    setShareData({link: data.toString()})
+    console.log('share.link:', data)
+   // let newParams = {...editParams.params};
+   // newParams.loading=false;
+   // setParams(newParams) ;
+    }
+  );
+}
+}
+);
+
+
+
   function ui(metaData: MetaData, onClickIn: (metaData: MetaData, ps: MultiShapeParams)=>void) : JSX.Element{
-    return <Card variant={"outlined"}>
+   console.log("shareData", shareData)
+   return <Card variant={"outlined"}>
     <CardContent>
       <Typography gutterBottom variant="h5" component="h2">
         {metaData.description}
       </Typography>
       <CardContent   sx={{
               bgcolor: 'background.paper',
-              boxShadow: 1,
               borderRadius: 2,
               p: 2,
               minWidth: 300,
@@ -70,10 +133,14 @@ console.log(`inEdit: ${inEdit} metaDatas: ${metaDatas}`, metaDatas)
   {inEdit && editParams &&
   <Fragment>
      <Button onClick={clear}>Clear</Button>
+     <Button onClick={onClickDownload}>Download</Button>
+     <Button onClick={onClickShare}>Share</Button>
+     {shareData && (shareData.link ?
+       <ShareView link={shareData.link}></ShareView> : <Button>building share link . . .</Button>) }
      <Edit metaData={editParams.metaData} params={editParams.params}/>
    </Fragment>
-  }
-  {!inEdit && editParams && <span padding-right= "20" >&nbsp;{editParams.metaData.render(small(editParams.params))}</span>}
+}
+{!inEdit && editParams && <span padding-right= "20" >&nbsp;{editParams.metaData.render(small(editParams.params))}</span>}
   {inEdit && !editParams &&  metaDatas.map(mt=>ui(mt, onClick))}
 
   </Fragment>
